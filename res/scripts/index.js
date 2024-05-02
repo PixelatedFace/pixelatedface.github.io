6889c37ecaa52fb6b3c6241a28a0b0d5bb984f62
Pixelated Face <105989373+PixelatedFace@users.noreply.github.com>
2024-05-02 13:38:21 +0300

    a

--- res/scripts/index.js
+++ res/scripts/index.js
@@ -13,8 +13,9 @@ const Engine = Matter.Engine,
 
 const path = './res/images/slugpup_';
 
-var engine = Engine.create(),
-    world = engine.world;
+var engine = Engine.create();
+
+var world = engine.world;
 
 var render = Render.create({
     element: document.body,
@@ -22,10 +23,13 @@ var render = Render.create({
     options: {
         width: 500,
         height: 500,
+        canvas: document.getElementById('canvas'),
         showAngleIndicator: false,
         wireframes: false
     }
 });
+
+render.canvas.style = 'position: absolute; bottom: 0; right: 0;';
 
 Render.run(render);
 
@@ -39,13 +43,26 @@ const radius = 8
 var head, body;
 var slugpup = softbody(100, 100, 1, 2, 0, 0, true, radius, part_opt, [path+'head.png', path+'body.png', path+'body.png']);
 
+const color = 'rgb(37, 32, 54)';
+
+var ground = Bodies.rectangle(250, 475, 500, 50, {isStatic: true, render: {fillStyle: color}});
+var ceiling = Bodies.rectangle(250, 25, 500, 50, {isStatic: true, render: {fillStyle: color}});
+var wall_l = Bodies.rectangle(25, 250, 50, 400, {isStatic: true, render: {fillStyle: color}});
+var wall_r = Bodies.rectangle(475, 250, 50, 400, {isStatic: true, render: {fillStyle: color}});
 
 
-var ground = Bodies.rectangle(250, 450, 500, 50, {isStatic: true});
-var ceiling = Bodies.rectangle(250, 25, 500, 50, {isStatic: true});
-var wall_l = Bodies.rectangle(0, 25, 500, 50, {isStatic: true});
-var wall_r = Bodies.rectangle(250, 25, 500, 50, {isStatic: true});
-Composite.add(world, [slugpup, ground, ceiling])
+Composite.add(world, [slugpup, ground, ceiling, wall_l, wall_r]);
+
+var boxes = [];
+
+for (let i = 0; i < 10; i++) {
+    boxes.push(Bodies.rectangle(250+randomInt(5), 250, 10, 10));
+}
+for (let i = 0; i < 5; i++) {
+    boxes.push(Bodies.rectangle(250+randomInt(5), 250, 25, 25));
+}
+
+Composite.add(world, boxes);
 
 var mouse = Mouse.create(render.canvas);
 
@@ -64,15 +81,14 @@ Composite.add(world, mouseConstraint);
 
 render.mouse = mouse;
 
-Render.lookAt(render, {
-    min: {x: 0, y: 0},
-    max: {x: 500, y: 500}
-});
-
 
 var runner = Runner.create();
 
 Runner.run(runner, engine);
+
+function randomInt(max){
+    return Math.floor(Math.random() * max);
+}
 
 
 // THANK YOU MATTERJS EXAMPLES
@@ -95,3 +111,23 @@ function softbody(_x, _y, columns, rows, c_gap, r_gap, brace, part_rad, part_opt
     return softBody;
 }
 
+var spamton = document.getElementById("spamton");
+
+spamton.onclick = function(){
+    spamton.id = null;
+    setInterval(function(){
+        spamton.id = "spamton"
+    }, 5000);
+}
+
+document.getElementById("btn_itch").onclick = function(){
+    open("https://pixelatedface.itch.io/")
+}
+
+document.getElementById("btn_git").onclick = function(){
+    open("https://github.com/PixelatedFace")
+}
+
+document.getElementById("btn_yt").onclick = function(){
+    open("https://www.youtube.com/channel/UCGw_0V2-_PxuoEaSzrxmRBQ")
+}
